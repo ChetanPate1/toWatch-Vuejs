d73a8e1135931064482f7b87d8162384b3de8503
ChetanPate1 <=>
2017-10-19 21:45:55 +0100

    init repo

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,11 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'NavigationBar'
+}
+</script>
+
+<style lang="scss" src="./components/NavigationBar.scss"></style>
---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import MostPopular from '@/components/MostPopular';
+import Login from '@/components/Login';
+import MyShows from '@/components/MyShows';
+import WatchList from '@/components/WatchList';
+import RewatchList from '@/components/RewatchList';
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/most-popular',
+      name: 'mostPopular',
+      component: MostPopular
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/my-shows',
+      name: 'myShows',
+      component: MyShows
+    },
+    {
+      path: '/watch-list',
+      name: 'watchList',
+      component: WatchList
+    },
+    {
+      path: '/rewatch-list',
+      name: 'rewatchList',
+      component: RewatchList
+    }
+  ]
+})
