ffabe6917c612a8b284cdf4166eeb4ec2221eae2
ChetanPate1 <=>
2018-03-03 20:33:56 +0000

    behind count button component added

---
+++ src/components/BehindCountButton/BehindCountButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button class="more-button" type="button" name="more" :class="{ 'open' : open }">
+    <span class="dripicons-plus"></span>
+    <span class="behind">{{ behindCount }}</span>
+  </button>
+</template>
+
+<script>
+  import { objSize, timeNow } from '@/js/helperFunctions';
+  export default {
+    name: 'BehindCountButton',
+    props: {
+      open: Boolean,
+      unWatched: Object,
+      currentSeason: String
+    },
+    computed: {
+      behindCount(){
+        let currentSeasonNum = parseInt(this.currentSeason, 0);
+        let seasonsLimit = objSize(this.unWatched) + currentSeasonNum;
+        let count = 0, j = 1, totalEpisodes = 0, season;
+        for (currentSeasonNum; currentSeasonNum < seasonsLimit; currentSeasonNum++) {
+          season = this.unWatched['season_' + currentSeasonNum];
+          totalEpisodes = objSize(season);
+          for (j; j < totalEpisodes; j++) {
+            if(!season[j].watched && season[j].airDate - timeNow() < 0){
+              count++;
+            }
+          }
+          j = 1;
+        }
+        return (count > 0) ? '-'+ count : count;
+      }
+    }
+  }
+</script>
+<style src="./BehindCountButton.scss" lang="scss"></style>
--- src/components/WatchlistCard/WatchlistCard.vue
+++ src/components/WatchlistCard/WatchlistCard.vue
@@ -1,10 +1,5 @@
 <template lang="html">
   <div class="watchlist-card" v-bind:style="{ 'background-image': 'url('+ imgSrc +')' }">
-     <!-- <button type="button" name="more" class="more-button" v-bind:class="{'open' : open, 'up-to-date': behindCount(seasons, currentseason) == 0 }" ng-click="toggleOpen('open')">
-        <span class="dripicons-plus"></span>
-        <span class="behind">{{ behindCount(seasons, currentseason) }}</span>
-     </button> -->
-
      <h2>{{ heading }}</h2>
      <h4>{{ details }}</h4>
      <h5>On {{ subHeading }}</h5>
