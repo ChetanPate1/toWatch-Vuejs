fcd39f9b9e308a904b829e0fd50b6b12529ac442
Chetan Patel <chetanpatel@Chetans-MacBook-Pro.local>
2018-06-09 22:50:43 +0100

    basic movies displayed

--- src/components/WatchedMovies.vue
+++ src/components/WatchedMovies.vue
@@ -1,8 +1,11 @@
 <template lang="html">
 <div class="container">
    <div class="row">
-      
-
+      <div class="col-md-4" v-for="movie in watchedMovies">
+         <div class="table-row">
+            {{ movie.title }}
+         </div>
+      </div>
       <no-content :message="noContentMessage" :condition="!watchedMovies"></no-content>
    </div>
 </div>
@@ -30,10 +33,6 @@ export default {
    },
    mounted() {
       this.$store.dispatch('getWatchedMovies');
-
-
-
-
    },
    components: {
       NoContent
@@ -42,4 +41,28 @@ export default {
 </script>
 
 <style lang="scss">
+.table-row{
+   display: inline-block;
+   width: 100%;
+   background-color: #ffffff;
+   border-radius: 6px;
+   padding: 15px 20px;
+   margin-bottom: 10px;
+   box-shadow: 0 8px 40px -10px rgba(0, 0, 0, 0.2);
+
+   .col-10{
+      float: left;
+      width: 10%;
+   }
+
+   .col-20{
+      float: left;
+      width: 20%;
+   }
+
+   .col-50{
+      float: left;
+      width: 50%;
+   }
+}
 </style>
