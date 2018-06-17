6f3fa0908bfce1db13fc500e7102f9156eb2fbae
Chetan Patel <chetanpatel@chetans-mbp.home>
2018-06-17 17:38:02 +0100

    popup can be open from outside component

--- src/components/Popup/Popup.vue
+++ src/components/Popup/Popup.vue
@@ -1,107 +1,28 @@
 <template lang="html">
-<div>
-   <button class="button pull-right margin-bottom-30" @click="open = true">Track a show</button>
-
-   <div class="popup-background" v-bind:class="{ 'open': open }" @click="open = false">
-      <div class="content" @click.stop>
-         <button class="close-button" @click.stop="open = false" type="button">
+   <div class="popup-background" v-bind:class="{ 'open': popupOpen }" @click="close()">
+      <div class="popup-content" @click.stop>
+         <button class="close-button" @click.stop="close()" type="button">
             <span class="dripicons-cross"></span>
          </button>
-
-         <form name="form" v-on:submit.prevent="add(form)" >
-            <div class="form-element">
-               <label for="series">Series Name</label>
-               <span class="dripicons-chevron-down"></span>
-               <select name="series" v-model="form.seriesRef" @change="form.season = ''; form.episode = ''">
-                  <option v-for="(show, key, index) in shows" v-bind:value="key"
-                     v-if="(show.status == 'In Development' || show.status == 'Running')" >
-                     {{ show.series }}
-                  </option>
-               </select>
-            </div>
-
-            <div class="form-element" v-if="form.seriesRef">
-               <label for="seasons">Seasons</label>
-               <div class="select-series">
-                  <div class="col-xs-3" v-for="(season, key, index) in shows[form.seriesRef].seasons">
-                     <label class="radio" v-bind:class="{'selected' : form.season >= index + 1 }" >
-                        <input type="radio" v-model="form.season" v-bind:value="index + 1" >
-                        {{ index + 1 }}
-                     </label>
-                  </div>
-                  <span class="selected-none" v-bind:class="{'selected': !form.seriesRef }">Select a season</span>
-               </div>
-            </div>
-
-            <div class="form-element" v-if="form.seriesRef && form.season">
-               <label for="seasons">Episodes</label>
-               <div class="select-series">
-                  <div class="col-xs-3" v-for="episode in shows[form.seriesRef].seasons[`season_${form.season}`]"
-                     v-if="checkAired(episode)">
-                     <label class="radio" v-bind:class="{'selected' : form.episode >= episode.number }" >
-                        <input type="radio" v-model="form.episode" v-bind:value="episode.number " >
-                        {{ episode.number }}
-                     </label>
-                  </div>
-                  <span class="selected-none" v-bind:class="{'selected': !form.season }">Select a episode</span>
-               </div>
-            </div>
-
-            <button class="button"
-               type="submit"
-               :disabled="!form.seriesRef || !form.season || !form.episode">
-               add
-            </button>
-         </form>
+         <slot></slot>
       </div>
    </div>
-</div>
-
 </template>
 
 <script>
-import { mapActions } from 'vuex';
-
 export default {
    name: 'Popup',
-   data() {
-      return {
-         open: false,
-         form: {
-            seriesRef: '',
-            season: '',
-            episode: ''
-         },
-         today: new Date().getTime()
+   data(){
+      return{
+         popupOpen: false
       }
    },
-   props: {
-      shows: Object
-   },
-   computed: {
-      ...mapActions([
-         'addToWatchlist'
-      ])
-   },
    methods: {
-      add(form){
-         this.$store.dispatch('addToWatchlist', form)
-            .then(() => {
-               this.open = false;
-               this.form = {
-                  seriesRef: '',
-                  season: '',
-                  episode: ''
-               };
-            });
+      open(){
+         this.popupOpen = true;
       },
-      checkAired(episode) {
-         if(typeof episode === 'object'){
-            let date = parseInt(episode.airDate);
-            return date - this.today < 0 || episode.number == 1;
-         }else {
-            return false;
-         }
+      close(){
+         this.popupOpen = false;
       }
    }
 }
@@ -116,17 +37,19 @@ export default {
    background-color: transparentize(#000000, 0.8);
    width: 100%;
    height: 100%;
-   transition: opacity 500ms ease;
+   transition: opacity 1000ms ease;
    -ms-transform: translate(100%, 0);
    transform: translate3d(100%, 0, 0);
 
-   .content{
+   .popup-content{
       position: absolute;
-      right: 50px;
-      top: 120px;
+      right: 0;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      margin: auto;
       display: block;
-      width: 100%;
-      max-width: 400px;
+      min-width: 400px;
       min-height: 200px;
       border-radius: 7px;
       padding: 40px 30px 20px 30px;
@@ -134,8 +57,9 @@ export default {
       border: 1px solid rgba(0, 0, 0, 0.05);
       box-shadow: 0 30px 65px -30px rgba(0, 0, 0, 0.3);
       transition: all 500ms ease;
-      -ms-transform: translate(0, -100%);
-      transform: translate3d(0, -100%, 0);
+      opacity: 0;
+      -ms-transform: translate(0, -50%);
+      transform: translate3d(0, -50%, 0);
 
       .close-button{
          position: absolute;
@@ -171,7 +95,8 @@ export default {
       -ms-transform: translate(0, 0);
       transform: translate3d(0, 0, 0);
 
-      .content{
+      .popup-content{
+         opacity: 1;
          -ms-transform: translate(0, 0);
          transform: translate3d(0, 0, 0);
       }
--- src/components/WatchList.vue
+++ src/components/WatchList.vue
@@ -1,11 +1,59 @@
 <template lang="html">
 <div class="container" v-bind:class="{'container-lg' : watchlistSize() > 3 }">
+   <popup ref="popup">
+      <form name="form" v-on:submit.prevent="add(form)">
+         <div class="form-element">
+            <label for="series">Series Name</label>
+            <span class="dripicons-chevron-down"></span>
+            <select name="series" v-model="form.seriesRef" @change="form.season = ''; form.episode = ''">
+               <option v-for="(show, key, index) in myShows" v-bind:value="key"
+                  v-if="(show.status == 'In Development' || show.status == 'Running')" >
+                  {{ show.series }}
+               </option>
+            </select>
+         </div>
+
+         <div class="form-element" v-if="form.seriesRef">
+            <label for="seasons">Seasons</label>
+            <div class="select-series">
+               <div class="col-xs-3" v-for="(season, key, index) in myShows[form.seriesRef].seasons">
+                  <label class="radio" v-bind:class="{'selected' : form.season >= index + 1 }" >
+                     <input type="radio" v-model="form.season" v-bind:value="index + 1" >
+                     {{ index + 1 }}
+                  </label>
+               </div>
+               <span class="selected-none" v-bind:class="{'selected': !form.seriesRef }">Select a season</span>
+            </div>
+         </div>
+
+         <div class="form-element" v-if="form.seriesRef && form.season">
+            <label for="seasons">Episodes</label>
+            <div class="select-series">
+               <div class="col-xs-3" v-for="episode in myShows[form.seriesRef].seasons[`season_${form.season}`]"
+                  v-if="checkAired(episode)">
+                  <label class="radio" v-bind:class="{'selected' : form.episode >= episode.number }" >
+                     <input type="radio" v-model="form.episode" v-bind:value="episode.number" >
+                     {{ episode.number }}
+                  </label>
+               </div>
+               <span class="selected-none" v-bind:class="{'selected': !form.season }">Select a episode</span>
+            </div>
+         </div>
+
+         <button class="button"
+            type="submit"
+            :disabled="!form.seriesRef || !form.season || !form.episode">
+            add
+         </button>
+      </form>
+   </popup>
+
    <div class="row">
       <div class="col-xs-6">
          <button class="button" type="button" name="button margin-bottom-10" @click="isEditMode = true">Manage</button>
       </div>
       <div class="col-xs-6">
-         <popup :shows="myShows"></popup>
+         <button class="button pull-right margin-bottom-30" @click="$refs.popup.open()">Track a show</button>
       </div>
 
       <div class="col-xs-12 col-sm-6 fade-in"
@@ -44,7 +92,14 @@ export default {
       return {
          today: new Date().getTime(),
          noContentMessage: 'Your watch list is empty!',
-         isEditMode: false
+         isEditMode: false,
+         popupOpen: false,
+         form: {
+            seriesRef: '',
+            season: '',
+            episode: ''
+         },
+         today: new Date().getTime()
       }
    },
    computed: {
@@ -55,7 +110,8 @@ export default {
       ...mapActions([
          'getWatchlist',
          'getMyShows',
-         'deleteWatchlist'
+         'deleteWatchlist',
+         'addToWatchlist'
       ])
    },
    mounted() {
@@ -89,6 +145,25 @@ export default {
          });
 
          return nextAired.length ? nextAired[0].airDate : 0;
+      },
+      add(form){
+         this.$store.dispatch('addToWatchlist', form)
+            .then(() => {
+               this.open = false;
+               this.form = {
+                  seriesRef: '',
+                  season: '',
+                  episode: ''
+               };
+            });
+      },
+      checkAired(episode) {
+         if(typeof episode === 'object'){
+            let date = parseInt(episode.airDate);
+            return date - this.today < 0 || episode.number == 1;
+         }else {
+            return false;
+         }
       }
    },
    components: {
