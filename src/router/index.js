a1c8723ea8752cbdc8a7d3a236cbc22bdcef655f
Chetan Patel <chetanpatel@chetans-mbp.home>
2018-08-05 00:49:00 +0100

    movie details xl poster reduced to natural width

--- src/router/index.js
+++ src/router/index.js
@@ -75,7 +75,10 @@ const router = new Router({
          }
       },
       { path: '/', redirect: '/most-popular' }
-   ]
+   ],
+   scrollBehavior (to, from, savedPosition) {
+     return { x: 0, y: 0 }
+   }
 });
 
 router.beforeEach((to, from, next) => {
